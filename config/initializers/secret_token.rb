d9e7999a644a9e73cc78701b47503bbcde6d885a
Marcelo De Polli <marcelo.polli@gmail.com>
2012-09-13 08:06:17 -0300

    Freshly generated scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HedraSite2::Application.config.secret_token = '5d82f36d4974184f7f95a0a0778b41847c82573d785d417101dbff960909172d4f774d3374d0ce163168b8e6311321f80eba89701598bf4002f8b6be15d502fc'
