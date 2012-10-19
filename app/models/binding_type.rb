927dc864f302d39ac87a746f7dfb8691df962c2b
Marcelo De Polli <marcelo.polli@gmail.com>
2012-10-18 21:45:04 -0300

    Add model relationships

--- app/models/binding_type.rb
+++ app/models/binding_type.rb
@@ -1,3 +1,7 @@
 class BindingType < ActiveRecord::Base
+  # Relationships
+  has_and_belongs_to_many :books
+
+  # Specify fields that can be accessible through mass assignment
   attr_accessible :name
 end
--- app/models/people.rb
+++
@@ -1,3 +0,0 @@
-class People < ActiveRecord::Base
-  attr_accessible :name
-end
