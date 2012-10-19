5084180cb7358fc0a56143583c1886ffc950e5da
Marcelo De Polli <marcelo.polli@gmail.com>
2012-10-18 22:13:38 -0300

    Fix more errors

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveRecord::Base
   # Relationships
   has_and_belongs_to_many :authors, :class_name => "Person", :association_foreign_key => :author_id
   has_and_belongs_to_many :translators, :class_name => "Person", :association_foreign_key => :translator_id
-  has_and_belongs_to_many :organizers, :class_name => "Person", :association_foreign_keyassociation_foreign_key => :organizer_id
+  has_and_belongs_to_many :organizers, :class_name => "Person", :association_foreign_key => :organizer_id
   has_and_belongs_to_many :editors, :class_name => "Person", :association_foreign_key => :editor_id
   has_and_belongs_to_many :illustrators, :class_name => "Person", :association_foreign_key => :illustrator_id
   has_and_belongs_to_many :languages
--- db/migrate/20121018234444_create_books.rb
+++ db/migrate/20121018234444_create_books.rb
@@ -1,7 +1,7 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-      t.string :name
+      t.string :title
       t.text :description
       t.float :width
       t.float :height
