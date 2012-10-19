1512498955b9f02870104ea3fb134b64f65bbdd2
Marcelo De Polli <marcelo.polli@gmail.com>
2012-10-18 22:08:00 -0300

    Fix error in migration

--- db/migrate/20121019010601_binding_types_books.rb
+++ db/migrate/20121019010601_binding_types_books.rb
@@ -2,7 +2,7 @@ class BindingTypesBooks < ActiveRecord::Migration
   def change
     create_table :binding_types_books, :id => false do |t|
       t.references :binding_type
-      t.references :books
+      t.references :book
     end
     add_index :binding_types_books, [:binding_type_id, :book_id]
   end
