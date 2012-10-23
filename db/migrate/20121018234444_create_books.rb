61ea14377c4d12659a6eb3444da814cfd2f8e4ba
Marcelo De Polli <marcelo.polli@gmail.com>
2012-10-23 14:10:48 -0200

    Change book attributes

--- db/migrate/20121018234444_create_books.rb
+++ db/migrate/20121018234444_create_books.rb
@@ -2,13 +2,14 @@ class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
       t.string :title
+      t.integer :pages
+      t.string :isbn
       t.text :description
       t.float :width
       t.float :height
       t.float :weight
       t.integer :edition
       t.datetime :released_at
-      t.integer :pages
 
       t.timestamps
     end
