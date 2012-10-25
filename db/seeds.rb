e512f8499aa47449ce3a1a7d5c75ee867e13fffd
Marcelo De Polli <marcelo.polli@gmail.com>
2012-10-25 14:03:54 -0200

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,86 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Encadernação
+
+binding_types = [
+  {
+    :name => "Brochura"
+  },
+  {
+    :name => "Capa dura"
+  },
+  {
+    :name => "Espiral"
+  }
+]
+
+# Idiomas
+
+languages = [
+  {
+    :name => "Português"
+  },
+  {
+    :name => "Inglês"
+  },
+  {
+    :name => "Finlandês"
+  }
+]
+
+# Tipos de participação
+
+roles = [
+  {
+    :name => "Autor"
+  },
+  {
+    :name => "Organização"
+  },
+  {
+    :name => "Coordenação"
+  },
+  {
+    :name => "Edição"
+  },
+  {
+    :name => "Ilustração"
+  },
+  {
+    :name => "Tradução"
+  },
+  {
+    :name => "Edição"
+  },
+  {
+    :name => "Prefácio"
+  },
+  {
+    :name => "Introdução"
+  },
+  {
+    :name => "Apresentação"
+  }
+]
+
+# Database operations
+
+binding_types.each do |attributes|
+  BindingType.find_or_initialize_by_name(attributes[:name]).tap do |t|
+    t.save!
+  end
+end
+
+languages.each do |attributes|
+  Language.find_or_initialize_by_name(attributes[:name]).tap do |t|
+    t.save!
+  end
+end
+
+roles.each do |attributes|
+  Role.find_or_initialize_by_name(attributes[:name]).tap do |t|
+    t.save!
+  end
+end
