eaa304a32546f1e18d6bd74423112ec3b29cbae9
Marcelo De Polli <marcelo.polli@gmail.com>
2012-10-26 18:15:39 -0200

    Removing secret_token.rb from gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HedraSite::Application.config.secret_token = 'ff886e6bb08ec0675f69b3f5887de8e147cbf6eaa783a16918c81939620ff249e124b7c642804ea4d9d6a0d1806b7e934a11af3a49bc6f5432c0c26c493e8d75'
