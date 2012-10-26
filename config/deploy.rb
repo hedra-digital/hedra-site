96784100a0372cf2567669fbdd95393b9ee9425a
Marcelo De Polli <marcelo.polli@gmail.com>
2012-10-26 18:12:30 -0200

    Capistrano setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,23 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "Hedra Site"
+set :repository,  "git@github.com:hedra-digital/hedra-site.git"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, :git
+set :user, 'deploy'
+
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "livrodaclasse_rsa")]
+
+set :use_sudo, false
+
+set :deploy_to, "/home/deploy/apps/staging"
+set :deploy_via, 'copy'
+
+default_run_options[:pty] = true
+
+role :web, "hedra.com.br"
+role :app, "hedra.com.br"
+role :db,  "hedra.com.br", :primary => true
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -23,3 +33,38 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+after 'deploy:update_code', 'deploy:symlink_db'
+
+namespace :deploy do
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+# namespace :deploy do
+#   namespace :assets do
+#     desc "Precompile assets on local machine and upload them to the server."
+#     task :precompile, :roles => :web, :except => { :no_release => true } do
+#       run_locally "bundle exec rake assets:precompile"
+#       find_servers_for_task(current_task).each do |server|
+#         run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
+#       end
+#     end
+#   end
+# end
+
+desc "Run the super-awesome rake task"
+task :super_awesome do
+  rake = fetch(:rake, 'rake')
+  rails_env = fetch(:rails_env, 'production')
+
+  run "cd '#{current_path}' && #{rake} super_awesome RAILS_ENV=#{rails_env}"
+end
