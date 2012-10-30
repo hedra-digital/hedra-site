465431c6e8c0e7c784b966e29d31a09f7630e8cc
Marcelo De Polli <marcelo.polli@gmail.com>
2012-10-30 15:39:50 -0200

    Fix uploader sprockets dependance

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -7,8 +7,8 @@ class CoverUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -23,7 +23,7 @@ class CoverUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
-    asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    asset_path("fallback/" + [version_name, "default.jpg"].compact.join('_'))
 
     # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
