48cf8ed0a1d6310867d6ed4699b8c6e83daae44d
Marcelo De Polli <marcelo.polli@gmail.com>
2012-10-31 13:35:11 -0200

    Helper for supplementary info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ module ApplicationHelper
   def flash_message
     messages = ""
     flash.each do |type, content|
-      messages = content_tag(:div, button_tag('&#215;'.html_safe, :type => 'button', :class => 'close', :'data-dismiss' => 'alert', :name => nil) + content, :class => "alert alert-#{ type == :notice ? "success" : "error" }")
+      button = button_tag('&#215;'.html_safe, :type => 'button', :class => 'close', :'data-dismiss' => 'alert', :name => nil) + content
+      messages = content_tag(:div, button, :class => "alert alert-#{ type == :notice ? "success" : "error" }")
     end
     messages
   end
--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -12,4 +12,31 @@ module BooksHelper
     return authors.join(', ')
   end
 
+  def book_supplementary_info(book)
+    urls = ""
+    urls = urls << content_tag(:dt, 'Número de páginas')
+    urls = urls << content_tag(:dd, book.pages)
+    if book.languages.present? && book.languages.count > 1
+      urls = urls << content_tag(:dt, 'Idiomas')
+      urls = urls << content_tag(:dd, book.languages.map { |x| x.name.downcase }.join(", ").capitalize)
+    end
+    if book.binding_type.present?
+      urls = urls << content_tag(:dt, 'Encadernação')
+      urls = urls << content_tag(:dd, book.binding_type.name)
+    end
+    if book.width.present? && book.height.present?
+      urls = urls << content_tag(:dt, 'Dimensões')
+      urls = urls << content_tag(:dd, "#{book.width} &times; #{book.height} cm".html_safe)
+    end
+    if book.weight.present?
+      urls = urls << content_tag(:dt, 'Peso')
+      urls = urls << content_tag(:dd, book.weight)
+    end
+    if book.released_at.present?
+      urls = urls << content_tag(:dt, 'Ano de lançamento')
+      urls = urls << content_tag(:dd, book.released_at.year)
+    end
+    urls.html_safe
+  end
+
 end
