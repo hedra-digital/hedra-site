4faa1946bbef977f9f362216f87e048f73a3ee9d
Marcelo De Polli <marcelo.polli@gmail.com>
2012-12-03 21:26:02 -0200

    Change released_at to date

---
+++ db/migrate/20121203232001_change_released_at_to_date.rb
@@ -0,0 +1,9 @@
+class ChangeReleasedAtToDate < ActiveRecord::Migration
+  def up
+    change_column :books, :released_at, :date
+  end
+
+  def down
+    change_column :books, :released_at, :datetime
+  end
+end
