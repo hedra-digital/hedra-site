c4698467a3dd4125e0675d8dc4fb39248a396e1e
Marcelo De Polli <marcelo.polli@gmail.com>
2012-12-11 15:02:21 -0200

    Remove item limit from recommendations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   def home
     @features = Feature.includes(:book).all.map(&:book).first(1)
     @new_releases = NewRelease.includes(:book).all.map(&:book).first(4)
-    @recommendations = Recommendation.includes(:book).all.map(&:book).first(6)
+    @recommendations = Recommendation.includes(:book).all.map(&:book)
   end
 
   def about
