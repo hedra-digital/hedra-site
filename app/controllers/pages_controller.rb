ba7ab73e384d5cf168244170b86289f9dfba3280
Marcelo De Polli <marcelo.polli@gmail.com>
2012-12-11 16:46:34 -0200

    Fix include bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
 
   def home
-    @features = Feature.includes(:books, :people).all.map(&:book).first(1)
-    @new_releases = NewRelease.includes(:book).all.map(&:book).first(4)
-    @recommendations = Recommendation.includes(:book).all.map(&:book)
+    @features = Feature.includes(:book => :people).map(&:book).first(1)
+    @new_releases = NewRelease.includes(:book => :people).map(&:book).first(4)
+    @recommendations = Recommendation.includes(:book => :people).map(&:book)
   end
 
   def about
