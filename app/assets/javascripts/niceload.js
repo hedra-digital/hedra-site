8274c4e73de8c1b431ab825706f9c200585e30b4
Marcelo De Polli <marcelo.polli@gmail.com>
2012-12-12 20:25:12 -0200

    Update Bootstrap to 2.2.2

--- app/assets/javascripts/niceload.js
+++
@@ -1,7 +0,0 @@
-bkLib.onDomLoaded(function() {
-  var myNicEditor = new nicEditor({
-    buttonList : ['bold','italic','underline','left','center','right','justify','ol','ul','indent','outdent','link','unlink','subscript','superscript','xhtml','removeformat'],
-    iconsPath : '/assets/editoricons.gif'
-  });
-  myNicEditor.panelInstance('nicedit-1');
-});
