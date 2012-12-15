4985147719bc3c504f8ed73d8eedeeabf944e832
Rodrigo Franco <caffeine@gmail.com>
2012-12-15 16:08:48 -0200

    Implements category

--- test/unit/book_test.rb
+++ test/unit/book_test.rb
@@ -10,12 +10,15 @@
 #  width           :float
 #  height          :float
 #  weight          :float
-#  released_at     :datetime
+#  released_at     :date
 #  binding_type_id :integer
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  slug            :string(255)
 #  cover           :string(255)
+#  price_print     :float
+#  price_ebook     :float
+#  category_id     :integer
 #
 
 require 'test_helper'
