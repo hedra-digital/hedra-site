1c7db26299e19ad7e2109d787e49c86a486ca0f6
Marcelo De Polli <marcelo.polli@gmail.com>
2012-12-18 00:39:44 -0200

    Add books to category edit page

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -3,6 +3,7 @@ ActiveAdmin.register Category do
   form do |f|
     f.inputs do
       f.input :name
+      f.input :books, :as => :check_boxes, :wrapper_html => { :class => "multicolumn1" }
     end
     f.buttons
   end
--- app/admin/tags.rb
+++ app/admin/tags.rb
@@ -1,9 +1,11 @@
 ActiveAdmin.register Tag do
-   form :html => { :enctype => "multipart/form-data" } do |f|
+
+  form do |f|
     f.inputs do
-      f.input :name, :label => "Nome"
-      f.input :books, :as => :check_boxes
+      f.input :name
+      f.input :books, :as => :check_boxes, :wrapper_html => { :class => "multicolumn1" }
     end
     f.buttons
   end
+
 end
