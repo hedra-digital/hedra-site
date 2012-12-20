0e30b88a1c2af3cf4fd16d829b355c8baabc5342
Rodrigo Franco <caffeine@gmail.com>
2012-12-20 18:26:56 -0200

    In the Category admin page, list the available books alphabetically and hide books already in a category/

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Category do
   form do |f|
     f.inputs do
       f.input :name
-      f.input :books, :as => :check_boxes, :wrapper_html => { :class => "multicolumn1" }
+      f.input :books, :as => :check_boxes, :wrapper_html => { :class => "multicolumn1" }, :collection => Book.where("category_id is NULL or category_id = ?", category.id).order("title ASC")
     end
     f.buttons
   end
