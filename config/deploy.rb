f50069536044e630b0891b5ac94223537526663c
Marcelo De Polli <marcelo.polli@gmail.com>
2012-12-20 21:38:36 -0200

    Add cap task to update search index

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ end
 #   end
 # end
 
-after 'deploy:update_code', 'deploy:symlink_db', 'deploy:symlink_uploads', 'deploy:copy_old_sitemap'
+after 'deploy:update_code', 'deploy:symlink_db', 'deploy:symlink_uploads', 'deploy:copy_old_sitemap', 'deploy:rebuild'
 after 'deploy:update', 'deploy:cleanup'
 # We need to run this after our collector mongrels are up and running
 # This goes out even if the deploy fails, sadly
@@ -58,6 +58,11 @@ namespace :deploy do
 
   task :copy_old_sitemap do
     run "if [ -e #{previous_release}/public/sitemap_index.xml.gz ]; then cp #{previous_release}/public/sitemap* #{current_release}/public/; fi"
+  end
+
+  desc "Rebuilds search index"
+  task :symlink_rebuild, :roles => :app do
+    run "rake ts:rebuild"
   end
 
   task :restart do
