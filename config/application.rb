fd5b843e78e395aba214c6f4b7e67a496b512021
Marcelo De Polli <marcelo.polli@gmail.com>
2012-12-21 20:06:34 -0200

    Add new CSS to precompile array

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module HedraSite
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += %w(ie8-and-down.css)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
