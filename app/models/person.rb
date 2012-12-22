270ab56974e43f61838c66b1b765312bf55774ca
Marcelo De Polli <marcelo.polli@gmail.com>
2012-12-22 17:44:31 -0200

    Add dependent destroy to participations from Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -10,7 +10,7 @@
 
 class Person < ActiveRecord::Base
   # Relationships
-  has_many :participations
+  has_many :participations, :dependent => :destroy
   has_many :books, :through => :participations
   has_many :roles, :through => :participations
 
