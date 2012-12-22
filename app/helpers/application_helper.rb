2741413ab1184f60aeec70e94ae33d28d6881d36
Marcelo De Polli <marcelo.polli@gmail.com>
2012-12-22 14:14:39 -0200

    Page metadata refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,9 +9,19 @@ module ApplicationHelper
     messages
   end
 
-  def title(page_title)
-    content_for(:h1, content_tag(:h1, page_title))
-    content_for(:title, content_tag(:title, page_title << ' | Editora Hedra'))
+  def page_data(options={})
+    content_for :og do
+      data = ""
+      data << tag(:meta, { :property => 'og:title', :content => options[:title] }) if options[:title].present?
+      data << tag(:meta, { :property => 'og:type', :content => options[:type] }) if options[:type].present?
+      data << tag(:meta, { :property => 'og:url', :content => "#{request.protocol}#{request.host_with_port}#{request.fullpath}"})
+      data << tag(:meta, { :property => 'og:image', :content => options[:image] }) if options[:image].present?
+      data << tag(:meta, { :property => 'fb:admins', :content => '694928618,534302953' })
+      data << tag(:meta, { :property => 'og:site_name', :content => 'Editora Hedra' })
+      raw(data)
+    end
+    content_for(:h1, content_tag(:h1, options[:title]))
+    content_for(:title, content_tag(:title, "#{options[:title]} | Editora Hedra")) if options[:title].present?
   end
 
   def formatted_list(array)
