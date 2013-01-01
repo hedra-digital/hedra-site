0ceaa7afdaaa4278a0b8167fbd56c940ce07aa26
Rodrigo Franco <caffeine@gmail.com>
2013-01-01 17:15:14 -0200

    Now showing the image in the Feature Admin form

--- app/admin/features.rb
+++ app/admin/features.rb
@@ -14,4 +14,12 @@ ActiveAdmin.register Feature do
     default_actions
   end
 
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs do
+      f.input :book
+      f.input :feature_image, :as => :file, :hint => (( f.object.new_record? || f.object.feature_image.nil? ) ? f.template.content_tag(:span, "nenhuma imagem") : f.template.image_tag(f.object.feature_image.url))
+    end
+    f.buttons
+  end
+
 end
