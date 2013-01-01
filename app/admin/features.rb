470630ae5f8344f402f7e6496bd1751906e9445e
Rodrigo Franco <caffeine@gmail.com>
2013-01-01 17:57:48 -0200

    Revamps features admin page

--- app/admin/features.rb
+++ app/admin/features.rb
@@ -6,18 +6,39 @@ ActiveAdmin.register Feature do
     end
   end
 
+  show do |f|
+    attributes_table do
+      row :book
+      row :feature_image do
+        image_tag(f.feature_image_url(:thumb)) if f.feature_image.present?
+      end
+      row :page do
+        f.page.tag.name if f.page_id.present?
+      end
+    end
+  end
+
   index do
     selectable_column
     column :id
-    column :book
+    column "Book" do |f|
+      f.book.present? ? f.book.title : "Destaque da tag #{f.page.tag.name}"
+    end
     column :updated_at
     default_actions
   end
 
   form :html => { :enctype => "multipart/form-data" } do |f|
     f.inputs do
-      f.input :book
-      f.input :feature_image, :as => :file, :hint => (( f.object.new_record? || f.object.feature_image.nil? ) ? f.template.content_tag(:span, "nenhuma imagem") : f.template.image_tag(f.object.feature_image.url))
+      
+      f.inputs "Destaque com livro" do
+        f.input :book
+      end
+      
+      f.inputs "Destaque sem livro" do
+        f.input :page, :collection => Page.all.map{|p| [p.tag.name, p.id] }
+        f.input :feature_image, :as => :file, :hint => (( f.object.new_record? || f.object.feature_image.nil? ) ? f.template.content_tag(:span, "nenhuma imagem") : f.template.image_tag(f.object.feature_image.url(:thumb)))
+      end
     end
     f.buttons
   end
