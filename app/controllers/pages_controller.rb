6f16e7007fe952637d7a2c29d656f36684773775
Rodrigo Franco <caffeine@gmail.com>
2013-01-01 17:16:01 -0200

    Implements views for image only features

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-    @features = Feature.includes(:book => { :participations => [:person, :role] }).map(&:book).first(1)
+    @features = Feature.includes(:book => { :participations => [:person, :role] }).first(1)
     @new_releases = NewRelease.includes(:book => { :participations => [:person, :role] }).map(&:book).first(4)
     @recommendations = Recommendation.includes(:book => { :participations => [:person, :role] }).map(&:book)
   end
