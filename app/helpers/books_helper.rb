ad8e7e2539f551340493b55a8c2ad4a0cf1e402a
Marcelo De Polli <marcelo.polli@gmail.com>
2013-01-09 10:38:01 -0200

    Change buy button label

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -39,7 +39,7 @@ module BooksHelper
       content_tag :ul, :class => 'buy' do
         content_tag :li do
           link_to add_to_cart_path(book), :method => :post, :class => 'btn btn-primary' do
-            concat content_tag(:span, 'Livro', :class => 'type')
+            concat content_tag(:span, 'Compre', :class => 'type')
             concat content_tag(:span, number_to_currency(book.price_print), :class => 'price')
           end
         end
