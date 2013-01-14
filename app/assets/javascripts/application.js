c8c396ddec186b2c3cc977bcd58ed71969d79f22
Marcelo De Polli <marcelo.polli@gmail.com>
2013-01-14 01:54:00 -0200

    Add book cover modal to book page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,6 @@
 //= require bootstrap-alert
 //= require bootstrap-dropdown
 //= require bootstrap-collapse
+//= require bootstrap-transition
+//= require bootstrap-modal
 //= require jquery.carouFredSel-6.1.0
