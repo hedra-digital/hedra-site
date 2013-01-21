e99751b5949df8e32b1518a74cdf2ddec08d131a
Marcelo De Polli <marcelo.polli@gmail.com>
2013-01-20 23:36:05 -0200

    Change staging to old Linode

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,2 @@
-server "96.126.125.185", :app, :web, :db, :primary => true
-set :deploy_to, "/home/deploy/apps/hedra-site"
+server "173.255.203.143", :app, :web, :db, :primary => true
+set :deploy_to, "/home/deploy/apps/staging"
