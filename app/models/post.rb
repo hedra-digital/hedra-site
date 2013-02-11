795d6233dfc26f75f4912b815081ac00610b1d9b
Marcelo De Polli <marcelo.polli@gmail.com>
2013-02-11 20:25:53 -0200

    Add FriendlyId to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,18 @@
 class Post < ActiveRecord::Base
-  attr_accessible :author_id, :body, :book_id, :published_at, :title
-  belongs_to :author, :class_name => "AdminUser", :foreign_key => "author_id"
-  belongs_to :book
+  extend FriendlyId
+  friendly_id                         :title, :use => :slugged
 
-  validates :author,       :presence => true
-  validates :body,         :presence => true
-  validates :title,        :presence => true
-  validates :published_at, :presence => true
+  # Relationships
+  belongs_to                          :author, :class_name => "AdminUser", :foreign_key => "author_id"
+  belongs_to                          :book
+
+  # Validations
+  validates :author,                  :presence => true
+  validates :body,                    :presence => true
+  validates :title,                   :presence => true
+  validates :published_at,            :presence => true
+  validates :slug,                    :uniqueness => true
+
+  # Specify fields that can be accessible through mass assignment
+  attr_accessible                     :author_id, :body, :book_id, :published_at, :title
 end
