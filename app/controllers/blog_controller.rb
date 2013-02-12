bb02e6276d59bb18ea9dad5433c06a67bd4c69cf
Marcelo De Polli <marcelo.polli@gmail.com>
2013-02-11 23:31:20 -0200

    Blog fixes

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,10 +1,10 @@
 class BlogController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.includes(:book => { :participations => [:person, :role] }).all
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.includes(:book => { :participations => [:person, :role] }).find(params[:id])
   end
 end
