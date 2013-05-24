698a38e00d58fbbaecfec3c156c272c0e4fbe72b
Marcelo De Polli <marcelo.polli@gmail.com>
2013-05-23 22:14:08 -0300

    Update capistrano production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 server 'hedra.com.br', :app, :web, :db, :primary => true
-set :deploy_to, "/home/deploy/apps/hedra-site"
+set :deploy_to, "/home/deploy/apps/hedrasite"
 after "deploy:update", "newrelic:notice_deployment"
