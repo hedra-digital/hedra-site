204464de345e3ffc2b8f9b10a353834bcee71d87
Fellipe <fellipe@vizir.com.br>
2013-07-17 18:47:23 -0300

    passando endereço para a parte 3 quando endereço já existe

--- app/assets/javascripts/address.js
+++ app/assets/javascripts/address.js
@@ -36,6 +36,8 @@ function showExistingAddress(id){
 }
 
 function fillAddress(data){
+	address_id = data.id;
+
 	var address_info = data.address + ', ' + data.number + ' ' + data.complement;
 	var region_info = data.district + ', ' + data.city + ' - ' + data.state;
 	var zipcode_info = 'CEP: ' + data.zip_code;
