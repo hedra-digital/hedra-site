0bfca26dfafa260e11b8e607a19fabf8ffe2a10f
Fabrício Ferrari de Campos <contato@vizir.com.br>
2013-09-16 18:54:27 -0300

    adicionado email do fabio com copia

--- db/schema.rb
+++ db/schema.rb
@@ -178,6 +178,7 @@ ActiveRecord::Schema.define(:version => 20130902204912) do
     t.text     "special_instructions"
     t.integer  "user_id"
     t.integer  "address_id"
+    t.integer  "book_id"
     t.datetime "created_at",                                                          :null => false
     t.datetime "updated_at",                                                          :null => false
   end
