565dace3d0c461fc61746e286cdd19ec06d6d023
Andy <andylinid45@users.noreply.github.com>
2014-07-09 14:40:44 +0800

    2.2 https://trello.com/c/lt3DRMeo/248-special-sales

---
+++ app/models/promotion.rb
@@ -0,0 +1,37 @@
+class Promotion < ActiveRecord::Base
+
+  belongs_to :book
+  belongs_to :tag
+  belongs_to :category
+  belongs_to :publisher
+
+  validates_presence_of :publisher, :started_at, :ended_at
+
+  validates :discount, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 1}
+
+  validate :validate_price_and_discount, :validate_book_tag_category, :started_at_ended_at
+
+  attr_accessible :book_id, :category_id, :discount, :ended_at, :price, :publisher_id, :started_at, :tag_id
+
+
+  private
+
+  def validate_price_and_discount
+    self.errors.add(:discount, "Price and discount can not be blank at the same time") if ( !self.price and !self.discount)
+    self.errors.add(:discount, "Price and discount can not be set at the same time") if ( self.price and self.discount)
+  end
+
+  def validate_book_tag_category
+    nil_count = 0
+    [self.book, self.tag, self.category].each do |attr|
+      nil_count += 1 unless attr
+    end
+
+    self.errors.add(:book, "Book, tag and category can not be blank at the same time") if nil_count == 3
+    self.errors.add(:book, "Book, tag and category can not be set at the same time") if nil_count == 0 or nil_count == 1
+  end
+
+  def started_at_ended_at
+    self.errors.add(:started_at, "End time is early than start time") if self.started_at and self.ended_at and self.ended_at <= self.started_at
+  end
+end
---
+++ db/migrate/20140706144303_create_promotions.rb
@@ -0,0 +1,16 @@
+class CreatePromotions < ActiveRecord::Migration
+  def change
+    create_table :promotions do |t|
+      t.float :discount
+      t.decimal :price, :precision => 8, :scale => 2, :default => 0.0, :null => false
+      t.integer :book_id
+      t.integer :tag_id
+      t.integer :category_id
+      t.integer :publisher_id, :null => false
+      t.datetime :started_at, :null => false
+      t.datetime :ended_at, :null => false
+
+      t.timestamps
+    end
+  end
+end
