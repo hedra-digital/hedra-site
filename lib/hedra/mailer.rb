441ae7c34cf1b50c810d34aac1a17ad8c327de18
Andy <andylinid45@users.noreply.github.com>
2014-07-09 13:41:06 +0800

    2.2 https://trello.com/c/nwzmho8a/253-extra-forgot-to-say-turn-order-info-compatible-with-labels

--- lib/hedra/mailer.rb
+++ lib/hedra/mailer.rb
@@ -1,11 +1,11 @@
-# -*- encoding : utf-8 -*-
 module Hedra
   class Mailer < ActionMailer::Base
-    default :from => "Editora Hedra<services@hedra.com.br>"
-
     def send_notification(params={})
       @order = Order.where(:id => params[:order_id]).includes([:order_items => :book]).first
-      mail(:to => params[:to], :subject => params[:subject],
+
+      publisher = @order.order_items.first.book.publisher
+      
+      mail(:from => "#{publisher.name}<#{publisher.contact_email}>", :to => params[:to], :subject => params[:subject],
         :template_path => params[:template_path],
         :template_name => params[:template_name],
         :bcc => params[:bcc])
