756fd06e9bdd19fef4fcd3bb51f5190ce49e8af6
Andy <andylinid45@users.noreply.github.com>
2014-07-26 16:55:57 +0800

    2.2 https://trello.com/c/uiyMDWa2/264-new-layout-for-scroll-pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,9 +18,9 @@ class PagesController < ApplicationController
     @tag   = Tag.where(:slug => params[:id]).first || not_found
     @page  = @tag.page
 
-    books_query = Book.joins(:tags).where("books.publisher_id = #{session[:publisher]} and tags.id = #{@tag.id}")
+    books_query = Book.joins(:tags).where("books.publisher_id = #{session[:publisher]} and tags.id = #{@tag.id}").order("books.position desc, books.id desc")
     books_count = books_query.count
     @highlight = books_query.first(4) if books_count >= 6
-    @books = books_query.order("books.position desc, books.id desc").paginate(:page => params[:page], :per_page => 10, :offset => (books_count >= 6 ? 4 : 0))
+    @books = books_query.paginate(:page => params[:page], :per_page => 10, :offset => (books_count >= 6 ? 4 : 0))
   end
 end
