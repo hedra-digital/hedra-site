6d5e738bfa6a8ab8a7239a2aa1f090d5f31ad365
Andy <andylinid45@users.noreply.github.com>
2014-08-01 21:34:51 +0800

    2.2 https://trello.com/c/BBjaTS0N/260-selling-control

--- app/admin/report.rb
+++ app/admin/report.rb
@@ -24,3 +24,35 @@ ActiveAdmin.register_page "Report" do
     render "index"
   end
 end
+
+
+
+ActiveAdmin.register_page "Report2" do
+
+  controller do
+    def index
+
+	    if params[:search]
+	      start_date = Date.parse(params[:search][:start_date]) unless params[:search][:start_date].blank?
+	      end_date = (Date.parse(params[:search][:end_date]) + 1.day) unless params[:search][:end_date].blank?
+	      user_name = params[:search][:user_name]
+	      user_email = params[:search][:user_email]
+	    end
+
+		  @transactions = Transaction.joins(:user).joins(:order).
+		    where(start_date ? ("transactions.updated_at > '#{start_date}'") : "").
+		    where(end_date ? ("transactions.updated_at < '#{end_date}'") : "").
+		    where(user_name.blank? ? "" : "users.name like '%#{user_name}%'").
+		    where(user_email.blank? ? "" : "users.email like '%#{user_email}%'").
+		    order("transactions.user_id, transactions.updated_at")
+    end
+  end
+
+  content do
+    render "index"
+  end
+end
+
+
+
+
