bc7afad76a8f9bcdf182aabb3fc2d13af69ab809
Andy <andylinid45@users.noreply.github.com>
2014-08-03 05:52:01 +0800

    2.2 https://trello.com/c/PV8oUfLx/286-coupons

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -2,7 +2,7 @@ class CouponController < ApplicationController
   def set_cookie
     promotion = Promotion.find_by_slug(params[:id])
     if promotion
-      cookies["coupon_#{promotion.id}".to_sym] = { value: promotion.slug, expires: promotion.ended_at }
+      cookies["coupon_#{promotion.id}".to_sym] = { value: promotion.slug, expires: promotion.ended_at, domain: :all }
     end
   	redirect_to "/"
   end
