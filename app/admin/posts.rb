121ce132a4abbfbc4f5e3c2ee633fd024e0c27d8
Andy <andylinid45@users.noreply.github.com>
2014-08-03 22:28:01 +0800

    2.2 https://trello.com/c/GqWhlQkX/327-bug-posting-on-blog

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Post  do
   form do |f|
     f.inputs do
       f.input :title
-      f.input :body, :as => :ckeditor
+      f.input :body
       f.input :published_at, :as => :datepicker
       f.input :author_id, :as => :hidden, :value => current_admin_user.id
       f.input :book, :as => :select, :collection => Book.order("title ASC").all
