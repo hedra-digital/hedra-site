ac6d21c97acf8cdad734e26f9bd443c990f13d61
Andy <andylinid45@users.noreply.github.com>
2014-10-16 20:22:42 +0800

    https://trello.com/c/MtTmZdeS/442-unable-to-edit-tag-page-when-have-first-picture-bug

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -7,8 +7,8 @@ ActiveAdmin.register Page  do
       f.input :tag
       f.input :person, :as => :select, :collection => Person.order(:name)
       f.input :body
-      f.input :tag_image, :as => :file, :hint => (( f.object.new_record? || f.object.tag_image.nil? ) ? f.template.content_tag(:span, "no photo yet") : f.template.image_tag(f.object.tag_image.url))
-      f.input :hero_image, :as => :file, :hint => (( f.object.new_record? || f.object.hero_image.nil? ) ? f.template.content_tag(:span, "no photo yet") : f.template.image_tag(f.object.hero_image.url))
+      f.input :tag_image, :as => :file, :hint => (( f.object.new_record? || f.object.tag_image.file.nil? ) ? f.template.content_tag(:span, "no photo yet") : f.template.image_tag(f.object.tag_image.url))
+      f.input :hero_image, :as => :file, :hint => (( f.object.new_record? || f.object.hero_image.file.nil? ) ? f.template.content_tag(:span, "no photo yet") : f.template.image_tag(f.object.hero_image.url))
     end
     f.buttons
   end
