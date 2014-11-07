61ce66470f2e92ad9c5686a5b03eee24fb2339a5
Andy <andylinid45@users.noreply.github.com>
2014-11-07 18:09:49 +0800

    https://trello.com/c/dXIGLnkr/334-ebook-offer-in-book-page-see-layout-ebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,6 @@ protected
     get_publisher
     get_categories
     get_cart_items
-    get_cart_price
   end
 
   def http_authenticate
@@ -35,25 +34,12 @@ protected
   end
 
   def get_cart_items
-    if session[:carrinho]
-      @cart_items ||= []
-      session[:carrinho].each do |book_id, quantity|
-        book = Book.includes(:participations => [:person, :role]).find(book_id)
-        @cart_items << [book, quantity]
-      end
-    end
-  end
-
-  def get_cart_price
-    if @cart_items
-      @cart_price = @cart_items.inject(0) { |sum, ele| sum += (view_context.show_price(ele[0]) * ele[1]) }
-    end
+    session[:cart] ||= []
   end
 
   def not_found
     raise ActiveRecord::RecordNotFound
   end
-
 
   def create_order(user, address_hash, cart, payment_method)
     return nil if cart.nil?
