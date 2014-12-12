9f36edda0c4159e3d8e71ae4f3bc987c8f237cd8
Andy <andylinid45@users.noreply.github.com>
2014-12-12 15:23:34 +0800

    https://trello.com/c/zdLOx6JR/485-urgent-email-field-in-csv-repport

--- app/admin/address.rb
+++ app/admin/address.rb
@@ -12,5 +12,19 @@ ActiveAdmin.register Address do
     column :state
     column :country
     default_actions                   
-  end                                 
+  end    
+
+  csv do
+    column :id
+    column ("user") {|a| a.user.name}
+    column ("email") {|a| a.user.email}
+    column :zip_code
+    column :address
+    column :number
+    column :complement
+    column :district
+    column :city
+    column :state
+    column :country
+  end                             
 end   
