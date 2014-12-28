311092050e9306e7416d3ce9edd51d6beb1c6a52
Andy <andylinid45@users.noreply.github.com>
2014-12-28 16:53:09 +0800

    https://trello.com/c/KY1ISTe3/499-standalone-sign-in-form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,22 @@
-# -*- encoding : utf-8 -*-
 class RegistrationsController < Devise::RegistrationsController
   def create
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
+    else
+    	error_messages = ""
+		  @user.errors.full_messages.each do |message|
+		    error_messages << "<li>#{message}</li>" 
+		  end 
+    	flash.alert = error_messages
     end
+
     respond_to do |format|
+    	format.html {redirect_to stored_location_for(:user) || root_path} if @user.errors.count == 0
+      format.html { redirect_to (new_session_path(resource_name)) } if @user.errors.count > 0
       format.js
     end
+
   end
 end
+
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,19 @@
-# -*- encoding : utf-8 -*-
 class SessionsController < Devise::SessionsController
 
   def create
     @user = User.where(:email => params[:user][:email]).first
     if !@user.nil? && @user.valid_password?(params[:user][:password])
       sign_in @user
+    else
+      flash.alert = "Informações de acesso inválidas."
     end
+
     respond_to do |format|
+      format.html {redirect_to stored_location_for(:user) || root_path} if @user
+      format.html { redirect_to (new_session_path(resource_name)) } unless @user
       format.js
     end
+
   end
 
   def destroy
