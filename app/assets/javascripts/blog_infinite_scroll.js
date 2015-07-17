c8fbe977905d48d3d457dcfc02f93bd1c4b8d8b2
Augusto Pedraza <augusto.pedraza08@gmail.com>
2015-07-17 07:30:36 -0300

    Added blog infinite scroll

---
+++ app/assets/javascripts/blog_infinite_scroll.js
@@ -0,0 +1,14 @@
+$(function() {
+  if ($('#main').size() > 0) {
+    return $(window).on('scroll', function() {
+      var more_posts_url;
+      more_posts_url = $('.pagination a[rel=next]').attr('href');;
+
+      if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
+        $('.pagination').html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />');
+        $.getScript(more_posts_url);
+      }
+      return;
+    });
+  }
+});
