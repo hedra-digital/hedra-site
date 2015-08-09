5bbcfaca2592fb07ed162bf99eb4bcce6a8c5432
Augusto Pedraza <augusto.pedraza08@gmail.com>
2015-08-09 08:30:48 -0300

    Added rails_email_preview gem support

---
+++ app/mailer_previews/notifier_preview.rb
@@ -0,0 +1,30 @@
+class NotifierPreview
+  def order_post_tracking
+    Notifier.order_post_tracking order
+  end
+
+
+  def mail_to_trello
+    Notifier.mail_to_trello order
+  end
+
+
+  def send_ebook
+    Notifier.send_ebook order
+  end
+
+
+  def order_created
+    Notifier.order_created Order.last
+  end
+
+
+  def order_completed
+    Notifier.order_completed order
+  end
+
+
+  def order_failed
+    Notifier.order_failed order
+  end
+end
