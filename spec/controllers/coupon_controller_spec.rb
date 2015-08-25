b69aeca5c54dff5d57ff0a7e89379eed8804ae29
Augusto Pedraza <augusto.pedraza08@gmail.com>
2015-08-25 07:20:30 -0300

    Added specs for CouponController

--- spec/controllers/coupon_controller_spec.rb
+++ spec/controllers/coupon_controller_spec.rb
@@ -5,8 +5,10 @@ describe CouponController do
     context "with a valid slug" do
       let(:slug) { "fAkE1-a222-ReAlLy-bIg3-SlUg-5555" }
       let(:ended_at) { DateTime.parse("2014-12-31 19:00:00") }
+      let(:link) { nil }
+
       let(:promotion) do
-        p = Promotion.new(slug: slug, ended_at: ended_at)
+        p = Promotion.new(slug: slug, ended_at: ended_at, link: link)
 
         allow(p).to receive(:id).and_return(1007)
         p
@@ -24,12 +26,25 @@ describe CouponController do
 
       it "sets the cookie experiation" do
         stub_cookie_jar = HashWithIndifferentAccess.new
-        controller.stub(:cookies) { stub_cookie_jar }
+        allow(controller).to receive(:cookies) { stub_cookie_jar }
 
         get :set_cookie, id: slug
 
         cookie = stub_cookie_jar["coupon_1007"]
         expect(cookie[:expires]).to eq(ended_at)
+      end
+
+      context "with a not blank link" do
+        let(:link) { "http://fake.url" }
+        before(:each) { get :set_cookie, id: slug }
+
+        it { is_expected.to redirect_to link }
+      end
+
+      context "with a blank link" do
+        before(:each) { get :set_cookie, id: slug }
+
+        it { is_expected.to redirect_to :root }
       end
     end
   end
