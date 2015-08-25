4afc0e82d5f2297ca1d08efe4d5c782c17b2b19c
Augusto Pedraza <augusto.pedraza08@gmail.com>
2015-08-25 07:25:24 -0300

    Added little refactoring

--- app/controllers/coupon_controller.rb
+++ app/controllers/coupon_controller.rb
@@ -1,14 +1,22 @@
 class CouponController < ApplicationController
   def set_cookie
     promotion = Promotion.find_by_slug(params[:id])
-    if promotion
-      cookies["coupon_#{promotion.id}".to_sym] = { value: promotion.slug, expires: promotion.ended_at, domain: :all }
+    redirect_target = :root
 
-      if !promotion.link.blank?
-      	redirect_to promotion.link
-      	return
-      end
+    if promotion
+      create_cookie_for promotion
+      redirect_target = promotion.link unless promotion.link.blank?
     end
-  	redirect_to "/"
+
+    redirect_to redirect_target
   end
+
+  private
+    def create_cookie_for(promotion)
+      cookies["coupon_#{promotion.id}".to_sym] = {
+        value: promotion.slug,
+        expires: promotion.ended_at,
+        domain: :all
+      }
+    end
 end
