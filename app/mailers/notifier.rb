e6bc4e6104875a2fd15f1a8eab37d69e656443da
Jonas Porto <jporto.contato@hotmail.com>
2016-04-02 20:14:08 -0300

    add deliver method to send partner email from Notifier class

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -30,7 +30,7 @@ class Notifier < ActionMailer::Base
   def order_completed(order)
     @order = order
     publisher = @order.order_items.first.book.publisher
-    PartnershipMailer.order_completed order
+    PartnershipMailer.order_completed(order).deliver
     mail(from: "#{publisher.name}<#{publisher.contact_email}>", to: order.user.email, subject: I18n.t('notifications.completed.subject'), bbc: "jorge@hedra.com.br")
   end
 
