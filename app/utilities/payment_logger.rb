1ce350b1bc7ddf073fa9c0b45b8d25c9e57faf4d
Jonas Porto <jporto.contato@hotmail.com>
2016-04-03 12:26:42 -0300

    indenting payment logger

--- app/utilities/payment_logger.rb
+++ app/utilities/payment_logger.rb
@@ -1,39 +1,38 @@
+# basic pretty print logger for payment errors purpopose
 class PaymentLogger 
-  
   def initialize
     @@logger ||= Logger.new("#{Rails.root}/log/payment_error.log")
   end
 
   class << self
+    def log(msg = '')
+      self.new
+      @@logger.info self.template msg
+    end
 
-	  def log(msg = '')
-	  	self.new
-	  	@@logger.info self.template msg
-	  end
+    def template(msg)
+      content =  fill_with_hash_delimiter
+      content << break_line
+      content << msg
+      content << break_line
+      content << fill_with_under_delimiter
+      content << break_line
+      content << caller.join("\n")
+      content << break_line
+      content << fill_with_hash_delimiter
+    end
 
-	  def template(msg)
-	  	content =  fill_with_hash_delimiter
-	  	content << break_line
-	  	content << msg
-	  	content << break_line
-	    content << fill_with_under_delimiter
-	    content << break_line
-	    content << caller.join("\n")
-	    content << break_line
-	    content << fill_with_hash_delimiter
-	  end
+    private
+    def break_line
+      "\n"
+    end
 
-	  private 
-	  def break_line
-	  	"\n"
-	  end
+    def fill_with_hash_delimiter
+      "#" * 110
+    end
 
-	  def fill_with_hash_delimiter
-	  	return "#" * 110
-	  end
-
-	  def fill_with_under_delimiter
-	  	return "_" * 110
-	  end
-	end
+    def fill_with_under_delimiter
+      "_" * 110
+    end
+  end
 end
